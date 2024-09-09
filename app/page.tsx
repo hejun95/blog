9c917a145d00d0bc67212d94491d95c9a4b28a8b
honghejun <1284023786@qq.com>
2024-09-09 22:08:33 +0800

    fix: 首页背景初始化

--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,24 @@
 "use client"
-
-import Image from "next/image";
+import Image  from "next/image";
 import { useEffect } from "react";
-import AOS from 'aos';
-import 'aos/dist/aos.css'
 
 export default function Home() {
   useEffect(() => {
-    AOS.init();
+    // AOS.init();
   }, [])
 
   return (
     <main className="w-svw h-svh overflow-x-hidden">
-      <div className="w-full font-mono">
+      <div className="w-full h-full font-mono">
         <p className="fixed z-10 left-44 top-20 w-auto text-4xl border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           我的博客-林休休是大美女
         </p>
         {[1, 2, 3, 4, 5].map((el) => (
-          <div style={{ display: 'block', width: '100%', height: '100%' }} key={el}>
-            <img
+          <div style={{ display: 'block', position:'relative', width: '100%', height: '100%' }} key={el}>
+            <Image
               src={`./home_${el}.jpg`}
               alt="home image"
-              style={{ width: '100%', height: '100%' }}
+              fill
             // data-aos="fade-up" data-aos-duration="800"
             />
           </div>
